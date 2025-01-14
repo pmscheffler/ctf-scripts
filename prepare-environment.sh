1968da69c18839414ce75d0b9eed9d39f3ba08f3
Peter Scheffler <p.scheffler@f5.com>
2025-01-14 10:17:11 -0500

    fixes

--- prepare-environment.sh
+++ prepare-environment.sh
@@ -2,4 +2,4 @@
 
 ctfd=$(curl --silent --location 'http://metadata.udf/deployment/components' --header 'Content-Type: application/json'| jq -r '.[] | .accessMethods.https[]? | select(.label == "CTFd") | .host' )
 
-echo 'CTFd: $host'
+echo 'CTFd: $ctfd'
